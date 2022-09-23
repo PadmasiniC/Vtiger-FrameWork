2c99e08cb3df1a4d15d17e97f98343ec8f57bc46
Padmasini <Padmasini@LAPTOP-PKPP5N4P>
2022-09-23 14:46:28 +0530

    commit-6

--- src/test/java/Vtiger/Practice/ReadDataFromcmdLine.java
+++ src/test/java/Vtiger/Practice/ReadDataFromcmdLine.java
@@ -6,11 +6,13 @@ public class ReadDataFromcmdLine {
 	@Test
 	public void readData()
 	{
+		
 		String BROWSER=System.getProperty("browser");
 		String URL=System.getProperty("url");
 		String USERNAME=System.getProperty("username");
 		String PASSWORD=System.getProperty("password");
 		
+		System.out.println("This is a Demo test");
 		System.out.println(BROWSER);
 		System.out.println(URL);
 		System.out.println(USERNAME);
